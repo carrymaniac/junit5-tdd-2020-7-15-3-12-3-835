804f207f54890fd40407f70ca30d05a93dc93a17
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-23 12:27:08 +0800

    [FizzBuzz][henry]feature: implement FizzBuzz play method given 21 then FizzWhizz

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     String play(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
+        } else if (number % 3 == 0 && number % 7 == 0) {
+            return "FizzWhizz";
         } else if (number % 3 == 0) {
             return "Fizz";
         } else if (number % 5 == 0) {
